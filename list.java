db287e0127bd824bfaf9fb5f024f24fa6e9d1035
Radhika Mishra <radhikamishra2413@gmail.com>
2025-07-08 01:23:11 +0530

    Return list without passing it to argument in java

--- list.java
+++ list.java
@@ -1,29 +1,26 @@
+
 // return list without passing it to argument 
-
 import java.util.ArrayList;
-public class list 
-{
-    public static void main(String args[])
-    {
 
-    int arr[]={2,3,1,4,4,5};
-    System.out.println(findallIndex2(arr, 4,0));
-    
-}
-static ArrayList<Integer> findallIndex2(int arr[],int target, int index)
-{
-ArrayList<Integer> list= new ArrayList<>();
-if ( index== arr.length)
-{
-    return list;
-}
-if(arr[index]==target)
-{
-    list.add(index);
-}
-    ArrayList<Integer> ansFromBelowCalls= findallIndex2( arr, target, index+1);
-    list.addAll(ansFromBelowCalls);
-    return list; 
+public class list {
+    public static void main(String args[]) {
 
-}
+        int arr[] = { 2, 3, 1, 4, 4, 5 };
+        System.out.println(findallIndex2(arr, 4, 0));
+
+    }
+
+    static ArrayList<Integer> findallIndex2(int arr[], int target, int index) {
+        ArrayList<Integer> list = new ArrayList<>();
+        if (index == arr.length) {
+            return list;
+        }
+        if (arr[index] == target) {
+            list.add(index);
+        }
+        ArrayList<Integer> ansFromBelowCalls = findallIndex2(arr, target, index + 1);
+        list.addAll(ansFromBelowCalls);
+        return list;
+
+    }
 }
