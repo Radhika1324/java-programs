354507a6f839872f654b2f6818a2ef630d8f4c45
Radhika Mishra <radhikamishra2413@gmail.com>
2025-07-08 00:54:12 +0530

    First occurance in java

--- firstoccurance.java
+++ firstoccurance.java
@@ -1,15 +1,10 @@
-class firstoccurence
-{
-    public int strStr(String haystack, String needle) 
-    {
-        if(haystack.length() < needle.length())
-        {
+class firstoccurence {
+    public int strStr(String haystack, String needle) {
+        if (haystack.length() < needle.length()) {
             return -1;
         }
-        for(int i=0; i<=haystack.length()-needle.length(); i++)
-        {
-            if(haystack.substring(i,i+needle.length()).equals(needle))
-            {
+        for (int i = 0; i <= haystack.length() - needle.length(); i++) {
+            if (haystack.substring(i, i + needle.length()).equals(needle)) {
                 return i;
             }
         }
