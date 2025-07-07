bede3ff140779304d3ddea48faa58d1f499812d8
Radhika Mishra <radhikamishra2413@gmail.com>
2025-07-08 01:34:08 +0530

    Swap two elements using function in java

--- swap.java
+++ swap.java
@@ -1,18 +1,21 @@
+
+//swap
 import java.util.*;
+
 public class swap {
-    void main()
-    {
-        int arr[]= {12,11,13,14,15};
+    void main() {
+        int arr[] = { 12, 11, 13, 14, 15 };
         swapit(arr, 0, 1);
 
-System.out.println(Arrays.toString(arr));
+        System.out.println(Arrays.toString(arr));
 
     }
-    static void swapit(int arr[], int index1, int index2){
-        int temp=arr[index1];
-        arr[index1]=arr[index2];
-        arr[index2]=temp;
+
+    static void swapit(int arr[], int index1, int index2) {
+        int temp = arr[index1];
+        arr[index1] = arr[index2];
+        arr[index2] = temp;
 
     }
-    
+
 }
