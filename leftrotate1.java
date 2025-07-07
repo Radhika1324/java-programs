8e2a228faee57cc1fc09bea6ace4352364610faa
Radhika Mishra <radhikamishra2413@gmail.com>
2025-07-08 00:43:58 +0530

    left rotate elements in java

--- leftrotate1.java
+++ leftrotate1.java
@@ -1,31 +1,27 @@
 import java.util.*;
-public class leftrotate1 
-{
-    public static void main(String args[])
-    {
-Scanner sc=new Scanner(System.in);
-int size=sc.nextInt();
-int arr[]=new int[size];
-for(int i=0; i<size; i++)
-{
-    arr[i]=sc.nextInt();
-}
-int k=sc.nextInt();
-for(int i=0; i<k; i++)
-{
-    int temp=arr[0],j;
-for(j=0; j<arr.length-1; j++)
-{
-    arr[j]=arr[j+1];
-}
 
-arr[j]=temp;
-}
+public class leftrotate1 {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+        int arr[] = new int[size];
+        for (int i = 0; i < size; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int k = sc.nextInt();
+        for (int i = 0; i < k; i++) {
+            int temp = arr[0], j;
+            for (j = 0; j < arr.length - 1; j++) {
+                arr[j] = arr[j + 1];
+            }
 
-for(int i=0; i<arr.length; i++)
-System.out.print(arr[i]+ " ");
-    
-    sc.close();
-}
-    
+            arr[j] = temp;
+        }
+
+        for (int i = 0; i < arr.length; i++)
+            System.out.print(arr[i] + " ");
+
+        sc.close();
+    }
+
 }
