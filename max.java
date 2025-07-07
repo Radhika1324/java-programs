41cbed900f9d0331ff4b3a9203636988f40a2796
Radhika Mishra <radhikamishra2413@gmail.com>
2025-07-08 01:24:15 +0530

    Find maximum element in an array in java

--- max.java
+++ max.java
@@ -1,24 +1,20 @@
-public class max
-{ 
-    public static void main(String args[])
-    {
-        int arr[]={1,2,14,158,5,14};
+//find max element 
+public class max {
+    public static void main(String args[]) {
+        int arr[] = { 1, 2, 14, 158, 5, 14 };
         System.out.println(maxx(arr));
     }
 
-    static int maxx(int arr[])
-    {
-        int maxVal=arr[0];
-        for(int i=1; i<arr.length; i++)
-        {
-            if(arr[i]>maxVal)
-            {
-                maxVal=arr[i];
+    static int maxx(int arr[]) {
+        int maxVal = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > maxVal) {
+                maxVal = arr[i];
             }
         }
 
-            return maxVal;
+        return maxVal;
 
     }
-    
+
 }
