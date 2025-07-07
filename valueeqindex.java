69b35b741bf6edc47912269ef2ced34fe677b86e
Radhika Mishra <radhikamishra2413@gmail.com>
2025-07-08 01:34:58 +0530

    Check value equal to index of array in java

--- valueeqindex.java
+++ valueeqindex.java
@@ -1,15 +1,15 @@
+
+//valueequalindex
 import java.util.*;
+
 class valueequalindex {
-    public List<Integer> valueEqualToIndex(List<Integer> nums) 
-    {
-        List<Integer> list=new ArrayList<>();
-        for(int i=0;i<nums.size();i++)
-        {
-            if(nums.get(i)== i+1)
-            {
+    public List<Integer> valueEqualToIndex(List<Integer> nums) {
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums.get(i) == i + 1) {
                 list.add(nums.get(i));
             }
-            
+
         }
         return list;
     }
