87c54ed76031d089a7afd9748c5e5fc6f325589e
Radhika Mishra <radhikamishra2413@gmail.com>
2025-07-08 01:29:01 +0530

    Sort an array in java

--- sort.java
+++ sort.java
@@ -1,17 +1,15 @@
+//sort ARRAY
 public class sort {
-    public static void main()
-    {
-        int arr[]={1,5,4,3};
+    public static void main() {
+        int arr[] = { 1, 5, 4, 3 };
         System.out.println(sorted(arr, 0));
     }
-    static boolean sorted(int arr[], int index)
-    {
-        if( index==arr.length-1)
-        {
+
+    static boolean sorted(int arr[], int index) {
+        if (index == arr.length - 1) {
             return true;
         }
-        return arr[index]<arr[index+1]&& sorted(arr, index+1);
+        return arr[index] < arr[index + 1] && sorted(arr, index + 1);
     }
 
-    
 }
