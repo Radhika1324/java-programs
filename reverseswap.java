3ae0d5eed7f0cffe8e6f13e2ecc8e0b66014c55f
Radhika Mishra <radhikamishra2413@gmail.com>
2025-07-08 01:28:01 +0530

    ReverseSwap in java

--- reverseswap.java
+++ reverseswap.java
@@ -1,28 +1,29 @@
+
+//reverseswap 
 import java.util.Arrays;
 
 public class reverseswap {
-    void main()
-    {
-        int arr[]={1,5,4,55,2};
+    void main() {
+        int arr[] = { 1, 5, 4, 55, 2 };
         reverse(arr);
         System.out.println(Arrays.toString(arr));
     }
-    static void reverse(int arr[])
-    { 
-        int start=0;
-        int end=arr.length-1;
-        while(start < end)
-        {
-            swapit(arr,start, end);
+
+    static void reverse(int arr[]) {
+        int start = 0;
+        int end = arr.length - 1;
+        while (start < end) {
+            swapit(arr, start, end);
             start++;
             end--;
         }
     }
-    static void swapit(int arr[], int index1, int index2){
-        int temp=arr[index1];
-        arr[index1]=arr[index2];
-        arr[index2]=temp;
+
+    static void swapit(int arr[], int index1, int index2) {
+        int temp = arr[index1];
+        arr[index1] = arr[index2];
+        arr[index2] = temp;
 
     }
 
-    }
+}
